5eba4741682023ff0c83323095c479752e504bd5
Josh Jacobs <joshalexjacobs@gmail.com>
2020-04-25 16:27:18 -0400

    added an entity spawner class that uses object pooling

---
+++ AthensJam2020/Assets/CameraMovementTest/Scripts/EntitySpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using CandyCoded;
+using UnityEngine;
+
+public class EntitySpawner : MonoBehaviour {
+
+    public bool spawnAsChild = false;
+    
+    [SerializeField]
+    [RangedSlider(0, 10)]
+    public RangedFloat randomWaitTimeRange;
+    
+    [SerializeField]
+    [RangedSlider(-10, 10)]
+    public RangedFloat randomXRange;
+    
+    [SerializeField]
+    [RangedSlider(-10, 10)]
+    public RangedFloat randomYRange;
+    
+    public CandyCoded.GameObjectPoolReference entityObjectPoolReference;
+
+    private void Awake() {
+        entityObjectPoolReference.parentTransform = transform;
+        entityObjectPoolReference.Populate();
+
+        StartCoroutine(SpawnEntity());
+    }
+
+    IEnumerator SpawnEntity() {
+        yield return new WaitForSeconds(randomWaitTimeRange.Random());
+        entityObjectPoolReference.Spawn(new Vector3(randomXRange.Random(), randomYRange.Random(), 0f), Quaternion.identity);
+        StartCoroutine(SpawnEntity());
+    }
+    
+}
