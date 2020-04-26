73791a934475603bfb2df975821bf7171c20cd97
Josh Jacobs <joshalexjacobs@gmail.com>
2020-04-26 11:44:43 -0400

    added player sprite

--- AthensJam2020/Assets/NewerStuff/JordanWork/PlayerInputActions.cs
+++ AthensJam2020/Assets/NewerStuff/JordanWork/PlayerInputActions.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/JordanWork/PlayerInputActions.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/NewerStuff/JordanWork/PlayerInputActions.inputactions'
 
 using System;
 using System.Collections;
