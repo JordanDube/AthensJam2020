85117c3a571ed44658413681107596e2768b4564
Josh Jacobs <joshalexjacobs@gmail.com>
2020-04-26 15:06:48 -0400

    ocean sounds

--- AthensJam2020/Assets/NewerStuff/JordanWork/Scripts/Puddle.cs
+++ AthensJam2020/Assets/NewerStuff/JordanWork/Scripts/Puddle.cs
@@ -1,8 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Puddle : MonoBehaviour {
+    
     public bool isInfinite = false;
     public float water = 12f;
+
+    private float maxWater;
+    private Animator _Animator;
+    
+    private void Awake() {
+        _Animator = GetComponent<Animator>();
+        maxWater = water;
+    }
+
+    private void Update() {
+        if (water < 4f) {
+            _Animator.SetBool("isLow", true);
+        } else if (water < 0f) {
+            _Animator.SetBool("isEmpty", true);
+        }
+    }
+    
 }
