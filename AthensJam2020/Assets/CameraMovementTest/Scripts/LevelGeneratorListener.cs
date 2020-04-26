bd01887c8817845ba0dc01f807116dab3896128d
Josh Jacobs <joshalexjacobs@gmail.com>
2020-04-26 01:51:27 -0400

    work on waves and manhole

---
+++ AthensJam2020/Assets/CameraMovementTest/Scripts/LevelGeneratorListener.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class LevelGeneratorListener : MonoBehaviour {
+
+  public float xDifference;
+  
+  public LevelGenerator levelGenerator;
+
+  private SpriteRenderer _SpriteRenderer;
+
+  private void Awake() {
+    _SpriteRenderer = GetComponent<SpriteRenderer>();
+    Debug.Log(Camera.main.ScreenToWorldPoint(Camera.main.transform.position));
+    Debug.Log(Camera.main.transform.position);
+  }
+
+  private void Update() {
+    if (_SpriteRenderer) {
+      if (_SpriteRenderer.isVisible) {
+        SpawnSegmentAndMoveTransform();
+      }
+    }
+  }
+
+  private void SpawnSegmentAndMoveTransform() {
+    transform.position = new Vector3(Camera.main.transform.position.x + xDifference, transform.position.y, 0f);
+    levelGenerator.SpawnAnotherSegment();
+  }
+
+}
